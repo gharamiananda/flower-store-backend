c5f578aabc639df38a3831d9bcfdf6880ed3c449
techwensananda <ananda.gharami@techwens.in>
2024-01-29 11:45:04 +0530

    Dcors

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,54 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import router from "./app/routes";
 import notFound from "./app/middlewares/notFound";
+import cors, { CorsOptions } from 'cors';
 
 const app: Application = express();
 
 //parsers
 app.use(express.json());
-app.use(cors({ credentials: true }));
+
+
+type OriginCheckFunction = (origin: string | undefined, callback: (error: Error | null, allow?: boolean) => void) => void;
+
+// Define the CORS options type
+interface CustomCorsOptions extends CorsOptions {
+  origin: OriginCheckFunction;
+}
+type Whitelist = string[];
+
+
+// Define the whitelist
+const whitelist: Whitelist = [
+  'http://localhost:3333',
+  'http://localhost:3000',
+  'http://localhost:3334',
+  'http://localhost:3001',
+  'http://knighthunt-website-admin.s3-website.ap-south-1.amazonaws.com',
+  'http://knighthunt-website.s3-website.ap-south-1.amazonaws.com',
+  'http://knighthunt-admin.techwens.in',
+  'http://knighthunt.techwens.in'
+];
+
+// Define the CORS options
+const corsOptions: CustomCorsOptions = {
+  origin(origin, callback) {
+    if (true) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+// Use the CORS middleware with custom options
+app.use(cors(corsOptions));
+// app.use(cors({ credentials: true }));
 
 
 // application routes
